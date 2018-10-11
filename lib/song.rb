95a1178e46b50e648ef4bbbaed46830b0c366db9
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-11 18:49:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,12 +10,9 @@ class Song
   @@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
-#  def self.find_by_name(name)
-#    @@songs.detect{|a| a.name == name}
-#  end
 
   def self.all
     @@songs
